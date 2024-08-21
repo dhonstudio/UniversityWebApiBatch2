503bebfe6e14b52beaaed10ca200123e5d5dafd2
Dian Muhammad Gufron <dian.gufron@kemenkeu.go.id>
2024-08-21 13:16:36 +0700

    add get all students feature

---
+++ Application/Features/StudentFeature/StudentFeature.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+
+namespace Application.Features.StudentFeature
+{
+    public class StudentFeature
+    {
+        private readonly IBaseRepository<Student> _studentRepository;
+
+        public StudentFeature(IBaseRepository<Student> studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return _studentRepository.GetAll();
+        }
+    }
+}
