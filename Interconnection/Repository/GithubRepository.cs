6aa8a5dc42f4139740d6616d7b9100b9528cbb80
Dian Muhammad Gufron <dian.gufron@kemenkeu.go.id>
2024-08-21 16:31:47 +0700

    add get user github

---
+++ Interconnection/Repository/GithubRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interconnection.Repository
+{
+    internal class GithubRepository : IGithubRepository
+    {
+        private const string BaseUrl = "https://api.github.com";
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public GithubRepository(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<GithubUser> GetUser(string username)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var user = await client
+                .GetFromJsonAsync<GithubUser>($"{BaseUrl}/users/{username}");
+
+            return user;
+        }
+    }
+}
