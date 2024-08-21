4558a69c93ef994bada62b986ec721f2220f5ba6
Dian Muhammad Gufron <dian.gufron@kemenkeu.go.id>
2024-08-21 16:35:15 +0700

    add add interconnection layer method

---
+++ Interconnection/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.Repositories;
+using Interconnection.Repository;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Interconnection
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddInterconnectionLayer(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddHttpClient();
+            services.AddSingleton<IGithubRepository, GithubRepository>();
+            return services;
+        }
+    }
+}
