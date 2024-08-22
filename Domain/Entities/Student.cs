acbb595f9461503a1f7a1e9d92b9c6436761a79e
dhonstudio <doon13@gmail.com>
2024-08-22 16:03:40 +0700

    fix sieve

--- Domain/Entities/Student.cs
+++ Domain/Entities/Student.cs
@@ -1,10 +1,13 @@
 ﻿using Domain.Common;
+using Sieve.Attributes;
 
 namespace Domain.Entities
 {
     public class Student : BaseEntity
     {
+        [Sieve(CanSort = true)]
         public int ID { get; set; }
+        [Sieve(CanFilter =true, CanSort =true)]
         public string FirstMidName { get; set; }
         public string LastName { get; set; }
         public DateTime EnrollmentDate { get; set; }
